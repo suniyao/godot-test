f246d2b8b7b4a323df983064efce6a822210f3b8
StephYSN <3011316681@qq.com>
2025-07-08 15:34:05 -0700

    project setting change, sprite change script, scene update

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -17,9 +16,9 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
 		velocity.x = direction * SPEED
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
